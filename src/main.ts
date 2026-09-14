74174d62a0f63131aa6c0c7c98ffda1c234cb78d
carina112001 <carina-lehwald@kabelmail.de>
2026-09-14 13:14:03 +0200

    feat: add card navigation

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,51 @@
-const message: string = "FlyingCards startset!"
-console.log(message);
+const card = document.querySelector(".card");
+
+const flashcards = [
+  {
+    question: "Was ist TypeScript?",
+    answer:
+      "Ein Programmiersprache, die JavaScript um statische Typen erweitert.",
+  },
+  {
+    question: "Was ist HTML?",
+    answer: "Eine Auszeichnungssprache zur Strukturierung von Webseiten.",
+  },
+  {
+    question: "Was ist CSS?",
+    answer: "Eine Stylesheet-Sprache zur Gestaltung von Webseiten.",
+  },
+];
+
+let currentIndex = 0;
+
+function showCurrentCard() {
+  if (card) {
+    card.textContent = flashcards[currentIndex]?.question ?? "";
+  }
+}
+
+showCurrentCard();
+
+function showNextCard() {
+  currentIndex = currentIndex + 1;
+
+  if (currentIndex >= flashcards.length) {
+    currentIndex = 0;
+  }
+  showCurrentCard();
+}
+
+function showPreviousCard() {
+  currentIndex = currentIndex - 1;
+  if (currentIndex < 0) {
+    currentIndex = flashcards.length - 1;
+  }
+  showCurrentCard();
+}
+
+card?.addEventListener("click", showNextCard);
+
+card?.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+  showPreviousCard();
+});
